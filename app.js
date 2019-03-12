9a9d2d0c9cc4d8a392920ca4c933eac80d9c25ca
liuhanshu2000 <liuhanshu2000@hotmail.com>
2019-03-11 17:06:25 -0700

    Added weui style library, safe area inset for ipX

--- app.js
+++ app.js
@@ -1,41 +1,25 @@
 //app.js
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
+  onLaunch: function() {
+    let that = this
+    wx.getSystemInfo({
+      success: function(res) {
+        that.globalData.isiPhoneX = /iphone x/i.test(res.model) ||
+          /iphone11/i.test(res.model);
+      },
+    })
+    console.log(that.globalData.isiPhoneX)
     // 登录
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况              
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }else{
-          console.log("Unauthed")
-        }
-      }
-    })
   },
+  
   globalData: {
     userInfo: null,
-    formTitle:"新生接机注册"
+    formTitle: "新生接机注册",
+    isiPhoneX: false
   }
 })
