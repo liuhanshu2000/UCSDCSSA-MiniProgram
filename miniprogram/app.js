5ed7bb18671875ed4a8605514186b2379396deb9
Xinpei Tan <t347100955@gmail.com>
2019-05-26 22:18:06 -0700

    Changed this app to cloud program

---
+++ miniprogram/app.js
@@ -0,0 +1,49 @@
+//app.js
+App({
+  onLaunch: function() {
+    let that = this
+    if (!wx.cloud) {
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+    } else {
+      wx.cloud.init({
+        traceUser: true,
+      })
+    }
+    wx.getSystemInfo({
+      success: function(res) {
+        that.globalData.isiPhoneX = /iphone x/i.test(res.model) ||
+          /iphone11/i.test(res.model);
+      },
+    })
+    // console.log(that.globalData.isiPhoneX)
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+  
+      }
+    })
+  },
+
+  /**
+   * Encapsulate wx.request()
+   */
+  requestGet(url, query) {
+    return new Promise((resolve, reject) => {
+      wx.request({
+        url: url,
+        data: query
+      , success(res){
+        resolve(res.data)
+      }, fail(e){
+        reject(e)
+      }})
+    })
+  },
+  
+  globalData: {
+    userInfo: null,
+    formTitle: "新生接机注册",
+    isiPhoneX: false
+  }
+})
---
+++ miniprogram/pages/form/form.js
@@ -0,0 +1,392 @@
+// pages/form/form.js
+let auth = require('flightXMLAuth')
+let app = getApp()
+let username = auth.username;
+let apiKey = auth.APIKey;
+const fxml_url = `http://${username}:${apiKey}@flightxml.flightaware.com/json/FlightXML2/FlightInfoEx`;
+import areaList from "./area.js";
+//Debounce utility
+const debounce = (fn, time) => {
+  let timeout;
+  return function() {
+    const functionCall = () => fn.apply(this, arguments);
+    clearTimeout(timeout);
+    timeout = setTimeout(functionCall, time);
+  }
+}
+
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    areaList: areaList,
+    showCityPopup: false,
+    showFlightPopup: false,
+    showHouseEnterPopup: false,
+    showHouseEndPopup: false,
+    showHostEnterPopup: false,
+    showHostEndPopup: false,
+    flightTimeText:"点击选择时间",
+    houseEnterTimeText:"点击选择时间",
+    houseEndTimeText:"点击选择时间",
+    hostEnterTimeText:"点击选择时间",
+    hostEndTimeText:"点击选择时间",
+    cityText:"点击选择城市",
+    flightStatus: false,
+    hotelStatus: false,
+    busStatus: false,
+    hostStatus: false,
+    isSharingRoom: false,
+    isTicketFrom: false,
+    empty:{ 
+      phone: true,
+      email:true,
+      flightNum: true
+    },
+    valid:{
+      phone:true,
+      email:true,
+      flightNum: true //航班号valid
+    },
+    visible:{
+      flightInfo: true,
+    },
+    planData: [
+      'simCard', 'bankCard', 'carPickUp', 'busPickUp', 'house', 'hostFamily'
+    ],
+    planName: [
+      '手机卡代办', '银行卡代办', 'LA轿车接机', 'LA大巴接机','酒店住宿', '寄宿家庭'
+    ],
+    planResult: [],
+    form: {
+      name: '',
+      phone: '',
+      usPhone:'',
+      weChat: '',
+      email:'',
+      flightInfo:{},
+      flightTime: Date.now(),
+      flightNum: 0, //接机人数
+      hotelNum: 0,
+      hotelRoomNum: 0,
+      houseEnterTime: Date.now(),
+      houseEndTime: Date.now(),
+      hostEnterTime: Date.now(),
+      hostEndTime: Date.now(),
+      dateTime: new Date(0),
+      city: [],
+    },
+    minDate: new Date().getTime(),
+    endMinDate: new Date().getTime(),
+  },
+
+  /**
+   * this function handles checkbox toggle events
+   */
+  onCheckChange(event) {
+    this.setData({
+      planResult: event.detail,
+    })
+  },
+  onFlightNumChange(event) {
+    this.setData({['form.flightNum']: event.detail})
+  },
+  onHotelNumChange(event) {
+    this.setData({['form.hotelNum']: event.detail})
+  },
+  onHotelRoomNumChange(event) {
+    this.setData({['form.hotelRoomNum']: event.detail})
+  },
+  /**
+   * this function handles checkbox cell click events
+   */
+  toggle(event) {
+    const { name } = event.currentTarget.dataset;
+    const checkbox = this.selectComponent(`.checkboxes-${name}`);
+    checkbox.toggle();
+    switch (name) {
+      case 'carPickUp':
+        this.setData({ flightStatus: !this.data.flightStatus });
+        break;
+      case 'house':
+        this.setData({ hotelStatus: !this.data.hotelStatus });
+        break;
+      case 'busPickUp':
+        this.setData({ busStatus: !this.data.busStatus });
+        break;
+      case 'hostFamily':
+        this.setData({ hostStatus: !this.data.hostStatus });
+        break;
+    }
+  },
+  onCityClickPopup() {
+    this.setData({
+      ['showCityPopup']: true
+    })
+  },
+  onCityPopupConfirm(event) {
+    this.setData({['form.city']:event.detail.values});
+    this.setData({ ['cityText']: this.data.form.city[0].name + '/' + this.data.form.city[1].name + '/' + this.data.form.city[2].name})
+    this.onCityPopupClose();
+  },
+  onCityPopupClose() {
+    this.setData({
+      ['showCityPopup']: false
+    })
+  },
+  onFlightClickPopup() {
+    this.setData({
+      ['showFlightPopup']: true
+    })
+  },
+  onFlightPopupConfirm(event) {
+    this.setData({['form.dateTime']:new Date(event.detail)})
+    this.setData({ ['flightTimeText']: this.data.form.dateTime.toString()})
+    this.onFlightPopupClose();
+  },
+  onFlightPopupClose() {
+    this.setData({
+      ['showFlightPopup']: false
+    })
+  },
+
+  onHouseEnterClickPopup() {
+    this.setData({
+      ['showHouseEnterPopup']: true
+    })
+  },
+  onHouseEnterPopupConfirm(event) {
+    this.setData({['form.houseEnterTime']:new Date(event.detail)})
+    this.setData({ ['houseEnterTimeText']: this.data.form.houseEnterTime.toString()})
+    this.setData({['endMinDate']: this.data.form.houseEnterTime.getTime()})
+    this.onHouseEnterPopupClose()
+  },
+  onHouseEnterPopupClose() {
+    this.setData({
+      ['showHouseEnterPopup']: false
+    })
+  },
+  onHouseEndClickPopup() {
+    this.setData({
+      ['showHouseEndPopup']: true
+    })
+  },
+  onHouseEndPopupConfirm(event) {
+    this.setData({['form.houseEndTime']:new Date(event.detail)})
+    this.setData({ ['houseEndTimeText']: this.data.form.houseEndTime.toString()})
+    this.onHouseEndPopupClose()
+  },
+  onHouseEndPopupClose() {
+    this.setData({
+      ['showHouseEndPopup']: false
+    })
+  },
+  onHostEnterClickPopup() {
+    this.setData({
+      ['showHostEnterPopup']: true
+    })
+  },
+  onHostEnterPopupConfirm(event) {
+    this.setData({['form.hostEnterTime']:new Date(event.detail)})
+    this.setData({ ['hostEnterTimeText']: this.data.form.hostEnterTime.toString()})
+    this.setData({['endMinDate']: this.data.form.hostEnterTime.getTime()})
+    this.onHostEnterPopupClose()
+  },
+  onHostEnterPopupClose() {
+    this.setData({
+      ['showHostEnterPopup']: false
+    })
+  },
+  onHostEndClickPopup() {
+    this.setData({
+      ['showHostEndPopup']: true
+    })
+  },
+  onHostEndPopupConfirm(event) {
+    this.setData({['form.hostEndTime']:new Date(event.detail)})
+    this.setData({ ['hostEndTimeText']: this.data.form.hostEndTime.toString()})
+    this.onHostEndPopupClose()
+  },
+  onHostEndPopupClose() {
+    this.setData({
+      ['showHostEndPopup']: false
+    })
+  },
+  onSharingChange ({ detail }) {
+    this.setData({ isSharingRoom: detail });
+  },
+  onTicketChange ({ detail }) {
+    this.setData({ isTicketFrom: detail });
+  },
+  onFieldChange(event) {
+    let that = this
+    switch (event.currentTarget.dataset.id) {
+      //switch using type of field id
+      //then set respective data field
+      case "name":
+        that.setData({
+          ["form.name"]: event.detail
+        })
+        break
+      case "phone":
+        that.setData({
+          ["form.phone"]: event.detail,
+          ["valid.phone"]:this.isValidPhone(event.detail)
+        })
+        break
+      case "weChat":
+        that.setData({
+          ["form.weChat"]: event.detail
+        })
+        break  
+      case "usPhone":
+        that.setData({
+          ["form.usPhone"]: event.detail,
+          ["valid.usPhone"]: this.isValidUSPhone(event.detail)
+        })  
+        break    
+      case "email":
+        that.setData({
+          ["form.email"]:event.detail,
+          ["valid.email"]:this.isValidEmail(event.detail)
+        })
+        break
+    }
+    
+  },
+
+  onFlightEnter({detail}) {
+    let that = this
+    if (!(/(\w){2}(\d){1,}/g.test(detail))){
+      that.setData({["valid.flightNum"]:false})
+      return;
+    }
+    that.setData({ ["valid.flightNum"]: true })
+    this.getFlightInfo(detail).then(res => {
+      if (res.hasOwnProperty('error')){
+        //No response from server
+        that.setData({['form.flightTime']: new Date(0)})
+      } else {
+        let eta = res.FlightInfoExResult.flights[0].estimatedarrivaltime
+        that.setData({['form.flightInfo']: res.FlightInfoExResult.flights[0]})
+        that.setData({['form.flightTime']: new Date(eta*1000)})
+        that.setData({['timePickerText']:that.data.form.flightTime.toString()})
+      }
+    }).catch(e => {
+      //TODO: Network Error
+      console.error(e)
+    })
+  },
+  
+  onSubmit: function(event){
+    console.log(this.data.form)
+    // 1. 获取数据库引用
+    const db = wx.cloud.database()
+    // 2. 构造查询语句
+    // collection 方法获取一个集合的引用
+    // where 方法传入一个对象，数据库返回集合中字段等于指定值的 JSON 文档。API 也支持高级的查询条件（比如大于、小于、in 等），具体见文档查看支持列表
+      // get 方法会触发网络请求，往数据库取数据
+      db.collection('books').where({
+        publishInfo: {
+          country: 'United States'
+        }
+      }).get({
+        success(res) {
+          // 输出 [{ "title": "The Catcher in the Rye", ... }]
+          console.log(res)
+        }
+      })
+    for(let k in this.data.valid){
+      if (!this.data.valid[k]){
+        console.log("There is Invalid Input")
+        return
+      }
+    }
+    let data = this.data.form
+    console.log("Submitted " + JSON.stringify(data))
+  },
+  /**
+   * Get flight information from
+   * flightXML APi
+   */
+  getFlightInfo: function (flightNumer) {
+    return new Promise((resolve, reject) => {
+      app.requestGet(fxml_url, {
+        ident: flightNumer,
+        howMany: 1
+      }).then(res => {
+        resolve(res)
+      })
+    })
+  },
+
+  isValidPhone: function(num){
+    return /1[34578]\d{9}/.test(num)
+  },
+
+  isValidUSPhone: function(num){
+    return /\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})/.test(num)
+  },
+
+  isValidEmail: function(email){
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]      {1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)
+  },
+
+  /** 
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+    this.onLoad()
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+
+})
